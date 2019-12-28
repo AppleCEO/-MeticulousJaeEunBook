6037f904e42d856d070877b814fc2048112cdd9c
도미닉 <kiljh@me.com>
2019-12-28 15:16:22 +0900

    MemoFormVC 에 아울렛과 엑션 연결

--- MyMemory/MyMemory/MemoFormVC.swift
+++ MyMemory/MyMemory/MemoFormVC.swift
@@ -9,22 +9,12 @@
 import UIKit
 
 class MemoFormVC: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBOutlet var contents: UITextView!
+    @IBOutlet var preview: UIImageView!
+    
+    @IBAction func save(_ sender: Any) {
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func pick(_ sender: Any) {
     }
-    */
-
 }
