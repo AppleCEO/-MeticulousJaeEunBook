a5afc72e7f13f8e6c651e53f07aeeae97655a4b5
도미닉 <kiljh@me.com>
2019-12-28 14:31:07 +0900

    모델로 사용할 MemoData 객체 구현

---
+++ MyMemory/MyMemory/MemoData.swift
@@ -0,0 +1,17 @@
+//
+//  MemoData.swift
+//  MyMemory
+//
+//  Created by joon-ho kil on 2019/12/28.
+//  Copyright © 2019 joon-ho kil. All rights reserved.
+//
+
+import UIKit
+
+class MemoData {
+    var memoIdx: Int?
+    var title: String?
+    var contents: String?
+    var image: UIImage?
+    var regdate: Date?
+}
